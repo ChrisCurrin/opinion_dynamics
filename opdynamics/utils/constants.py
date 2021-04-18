1de36c67e2e4fb5bf36776ba5d317d064fe490d5
Christopher <chris.crisur@gmail.com>
2021-04-18 17:31:53 +0200

    feat(constants): added in-degree and color constants

--- opdynamics/utils/constants.py
+++ opdynamics/utils/constants.py
@@ -20,6 +20,15 @@ DEFAULT_COMPRESSION_LEVEL = 7
 # has less contrast than seismic and the middle is more grey
 OPINIONS_CMAP = "coolwarm_r"
 INTERACTIONS_CMAP = "magma_r"
+LIGHT_BLUE = "#1ACAD9"
+LIGHT_RED = "#D9258E"
+LIGHT_GREEN = "#0FD977"
+TAN = "#BB9832"
+
+PRE_RDN_COLOR = LIGHT_RED
+POST_RDN_COLOR = LIGHT_BLUE
+POST_RECOVERY_COLOR = TAN
+DELTA_RDN_COLOR = LIGHT_GREEN
 
 # ----------
 # symbols
@@ -37,3 +46,5 @@ MEAN = MU = "$\mu$"
 PEAK_DISTANCE = math_fix(f"$\Lambda_{OPINION_SYMBOL}$")
 PEAK_DISTANCE_MEAN = math_fix(f"$\\langle {PEAK_DISTANCE} \\rangle$")
 PEAK_DISTANCE_VAR = math_fix(f"$\\sigma^2_{{{PEAK_DISTANCE}}}$")
+IN_DEGREE_SYMBOL = "$→ i$"
+OUT_DEGREE_SYMBOL = "$i →$"
